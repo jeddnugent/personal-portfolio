8d7eb4a1bbefc8c026b2908f2be35b26d542e1d0
Jedd Nugent <jeddnugent@gmail.com>
2025-08-22 13:17:51 +1000

    Adding Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Header from './components/Header'
 import './styles/App.css'
 
 function App() {
 
   return (
     <>
-
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+import "../styles/Header.css"
+import SunnyIcon from '@mui/icons-material/Sunny';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Header() {
+	return (
+		<>
+			<div className="container header">
+				<nav>
+					<ul className="header__menu">
+						<li>
+							<a className="header__link" href="#about">About</a>
+						</li>
+						<li>
+							<a className="header__link" href="#featured">Work</a>
+						</li>
+						<li>
+							<a className="header__link" href="#contact">Contact</a>
+						</li>
+						<li className="header__line"></li>
+						<li>
+							<button className="header__sun">
+								<SunnyIcon />
+							</button>
+						</li>
+						<li>
+							<a className="header__resume btn" href="#">Resume</a>
+						</li>
+					</ul>
+					<button className="header__bars">
+						<MenuIcon />
+					</button>
+				</nav>
+			</div>
+
+		</>
+	);
+}
+
+export default Header;
