6c6dd0f641b2bc98e9e2357225fe8eefbfbbd03e
Jedd Nugent <jeddnugent@gmail.com>
2025-08-22 15:03:33 +1000

    Create Hero.tsx

---
+++ src/components/Hero.tsx
@@ -0,0 +1,19 @@
+import "../styles/Hero.css";
+
+function Hero() {
+	return (
+		<>
+			<div className="hero container">
+				<img className="hero__img" src="/headshot.jpeg" alt="profile Picture" />
+				<h2 className="hero__subtitle">Hi, I'm Jedd 👋</h2>
+				<h1 className="hero__title">Software Developer</h1>
+				<p className="hero__desc">A passionate <strong>Software Developer</strong> with experience working in <strong>front-end mobile app development </strong>
+					as well as <strong>Front-end and Full Stack Websites/Applications</strong></p>
+				<a className="hero__btn btn" href="#">React out</a>
+			</div>
+		</>
+	);
+
+}
+
+export default Hero;
