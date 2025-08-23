194e1633d23c61ee17ae8677f92367c74bda9ccf
Jedd Nugent <jeddnugent@gmail.com>
2025-08-23 18:05:30 +1000

    Adding year and copyright to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
 	return (
 		<>
 			<footer className='footer container section'>
-				<h3 className="footer__title">~ Jedd Nugent ~</h3>
+				<h3 className="footer__title">~ © {new Date().getFullYear()} Jedd Nugent ~</h3>
 			</footer>
 		</>
 	);
