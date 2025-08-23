19911042b13d267cc9768384c18e70fbd17d57f4
Jedd Nugent <jeddnugent@gmail.com>
2025-08-23 18:01:22 +1000

    Implementing Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import About from './components/About';
 import Contact from './components/Contact';
 import Featured from './components/Featured';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import './styles/App.css';
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Featured />
       <Contact />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import '../styles/components/Footer.css';
+
+function Footer() {
+	return (
+		<>
+			<footer className='footer container section'>
+				<h3 className="footer__title">~ Jedd Nugent ~</h3>
+			</footer>
+		</>
+	);
+}
+
+export default Footer;
