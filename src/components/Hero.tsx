34edfe27e3eaf8de3123d7f81873a8ae4606e1a2
Jedd Nugent <jeddnugent@gmail.com>
2025-08-27 15:16:18 +1000

    Squashed commit of the following:
    commit 8831f143d1a071b2fdc95ef304203411d59fc0d4
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Wed Aug 27 15:15:32 2025 +1000
    
        Update <strong> usage in hero text
    
    commit e3788c7049ac8a2c53760138b711a7542e44da71
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Wed Aug 27 15:15:17 2025 +1000
    
        Removing 100% width from base containers
    
        fixing scrollable issue on mobile
    
    commit b8489c081cad00975fbc3d24b927b193b4967083
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Wed Aug 27 15:08:43 2025 +1000
    
        Implementing Mobile Menu functionality
    
    commit 39c0227206ecacf146a57f95bd1146a0783ce3ff
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Wed Aug 27 14:30:23 2025 +1000
    
        Changing website theme colours
    
        Deepening both the yellow and pink used to improve readability
    
    commit f91692547d348e3e3bd5f4184423da1597a50204
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Wed Aug 27 14:29:48 2025 +1000
    
        Altering text colouring
    
        Changing text colouring to be lighter in an effort to boost readability
    
    commit 1b14f5cbcbf7ce829cf908a2a89f116fdeeca484
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Wed Aug 27 14:28:38 2025 +1000
    
        About description copy update
    
    commit 89800ea7bac744186967a708fa78c68d9298ed54
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 20:59:54 2025 +1000
    
        Feature copy
    
    commit 39ce240d2bc7ba5c25a7517d835864ec04babdc5
    Merge: fb5a53a ebdeb5e
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 20:59:31 2025 +1000
    
        Merge branch 'main' into development
    
    commit fb5a53a667cb63917976d4aa116081a64f558ad8
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 18:26:09 2025 +1000
    
        Adding resume to be downloadable
    
    commit 87d7f870acf987e2fb1368d53d60ad912939dc60
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 18:25:53 2025 +1000
    
        Updating Copy
    
    commit e8ba6d539e4f8b104140d3f806604f4d2de706da
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 18:16:49 2025 +1000
    
        Adding ids to components for nav bar scrolling
    
    commit 57392cacb70de83b4c30bd3a3a0a481420927d21
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 18:14:32 2025 +1000
    
        Adding coder emoji as website icon
    
        + removing unused react.svg
    
    commit 194e1633d23c61ee17ae8677f92367c74bda9ccf
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 18:05:30 2025 +1000
    
        Adding year and copyright to footer
    
    commit 19911042b13d267cc9768384c18e70fbd17d57f4
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 18:01:22 2025 +1000
    
        Implementing Footer
    
    commit 4cccbd1ebf4aa1d7ff975cf8fb23455c5829e527
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 17:56:42 2025 +1000
    
        Updating copy for about section
    
    commit 7bc9342d01b41a2f8ca5a92c976f799453744d89
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 17:56:27 2025 +1000
    
        Implementing Contact
    
        + adding mailto link to hero section
    
    commit 57d138673a49da56551829f09ece399f799fdc45
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 17:34:04 2025 +1000
    
        Adding SMI link to featured section
    
    commit 11afbec0559c918bfc76a804a294a20253c46511
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 16:05:55 2025 +1000
    
        Implementing featured section
    
    commit a4e8caf1a4f47b02cc115773463b7252f9684f4d
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 16:05:34 2025 +1000
    
        Adding SMI food journal screenshots
    
    commit 798d25afa47f71753ae14f367af18336e3abf0d8
    Author: Jedd Nugent <jeddnugent@gmail.com>
    Date:   Sat Aug 23 14:51:35 2025 +1000
    
        Adding components folder inside styles for better organisation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,8 +7,7 @@ function Hero() {
 				<img className="hero__img" src="/headshot.jpeg" alt="Profile Picture Headshot" />
 				<h2 className="hero__subtitle">Hi, I'm Jedd 👋</h2>
 				<h1 className="hero__title">Software Developer</h1>
-				<p className="hero__desc">A passionate <strong>Software Developer</strong> located in Melbourne, Australia with experience working in <strong>front-end mobile app development </strong>
-					as well as <strong>Front-end and Full Stack Websites/Applications</strong></p>
+				<p className="hero__desc">A passionate <strong>Software Developer</strong> located in Melbourne, Australia with experience working in <strong>front-end mobile app development </strong> as well as <strong>Front-end</strong> and <strong>Full Stack Websites/Applications</strong></p>
 				<a className="hero__btn btn" href="mailto:jeddnugent@gmail.com" target="_blank">React out</a>
 			</div>
 		</>
