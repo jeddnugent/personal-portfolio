82897932298a7192b0dc0d89382c4099ac5446f9
Jedd Nugent <jeddnugent@gmail.com>
2025-09-03 12:34:34 +1000

    Implementing Project and Project tile sections of the website
    - Project tiles were made with the option of modular expansion upon the completion of further client work

---
+++ src/components/ProjectTile.tsx
@@ -0,0 +1,27 @@
+import "../styles/components/ProjectTile.css";
+
+function ProjectTile(props: {
+	projectTitle: string;
+	propjectId: string;
+	projectImg: string;
+	projectDesc: string;
+	projectLink: string;
+}) {
+	return (
+		<>
+			<h3 className="project-tile__title">{props.projectTitle}</h3>
+			<div className={`project-tile__img-wrapper project-tile__image${props.propjectId}`}>
+				<img src={props.projectImg} alt={props.projectTitle + "website"} />
+			</div>
+			<div className={`project-tile project-tile__content${props.propjectId}`}>
+				<h3 className="project-tile__subtitle">{props.projectTitle}</h3>
+				<p className="project-tile__desc">
+					{props.projectDesc}
+				</p>
+				<a href={props.projectLink} className="btn">Website</a>
+			</div>
+		</>
+	);
+}
+
+export default ProjectTile;
