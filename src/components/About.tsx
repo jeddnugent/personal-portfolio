605e7c9ff61128f36db5521bf21e4f26ba1d97ea
Jedd Nugent <jeddnugent@gmail.com>
2025-09-17 12:37:41 +1000

    Reducing viewport threshold for animations

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,7 +8,7 @@ function About() {
 				initial={{ opacity: 0, y: 50 }}
 				whileInView={{ opacity: 1, y: 0 }}
 				transition={{ duration: 0.6, ease: "easeOut" }}
-				viewport={{ once: true, amount: 0.3 }}
+				viewport={{ once: true, amount: 0.1 }}
 			>
 				<div id="about" className="about container section">
 					<div className="about__content">
--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -9,7 +9,7 @@ function Featured() {
 				initial={{ opacity: 0, y: 50 }}
 				whileInView={{ opacity: 1, y: 0 }}
 				transition={{ duration: 0.6, ease: "easeOut" }}
-				viewport={{ once: true, amount: 0.3 }}
+				viewport={{ once: true, amount: 0.1 }}
 			>
 				<div id="featured" className="featured container section">
 					<h3 className="featured__subtitle">Featured Project</h3>
